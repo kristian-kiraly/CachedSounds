277e83eda65d97033479e642ffbc3fc6694ad748
Kristian Kiraly <kristian.kiraly@gmail.com>
2023-10-26 22:21:11 -0700

    Initial Commit

--- Sources/CachedSounds/CachedSounds.swift
+++ Sources/CachedSounds/CachedSounds.swift
@@ -1,2 +1,59 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  CachedSounds.swift
+//
+//  Created by Kristian Kiraly on 8/10/23.
+//
+
+import AVFoundation
+
+public class CachedSounds {
+    private static var audioPlayers:[URL : AVAudioPlayer?] = [:]
+    
+    public static func playSounds(soundfile: String) {
+        DispatchQueue.main.async {
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback)
+                do {
+                    try AVAudioSession.sharedInstance().setActive(true)
+                    //print("AVAudioSession is Active")
+                } catch _ as NSError {
+                    //print(error.localizedDescription)
+                }
+            } catch _ as NSError {
+                //print(error.localizedDescription)
+            }
+            if let path = Bundle.main.path(forResource: soundfile, ofType: nil){
+                DispatchQueue.main.async {
+                    do {
+                        let url = URL(fileURLWithPath: path)
+                        var somePlayer:AVAudioPlayer?
+                        if let player = audioPlayers[url] {
+                            somePlayer = player
+                        } else {
+                            somePlayer = try AVAudioPlayer(contentsOf: url)
+                            audioPlayers[url] = somePlayer
+                        }
+                        somePlayer?.prepareToPlay()
+                        somePlayer?.play()
+                    } catch {
+                        print("Error")
+                    }
+                }
+            }
+        }
+    }
+    
+    public static func preloadSound(soundfile:String) {
+        guard let path = Bundle.main.path(forResource: soundfile, ofType: nil) else { return }
+        DispatchQueue.global().async {
+            do {
+                let url = URL(fileURLWithPath: path)
+                if audioPlayers[url] == nil {
+                    audioPlayers[url] = try AVAudioPlayer(contentsOf: url)
+                }
+            } catch {
+                print("Error")
+            }
+        }
+    }
+}
